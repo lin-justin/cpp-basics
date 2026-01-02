1b469f73c421ea89867790939b2383c9b8f50409
lin-justin <42877999+lin-justin@users.noreply.github.com>
2026-01-02 14:41:00 -0500

    add quiz question 3 program folder and code

---
+++ scope_duration_linkage/q_03/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+Write a function int accumulate(int x).
+This function should return the sum of all of the values of x that have been
+passed to this function.
+*/
+#include <iostream>
+
+int accumulate(int x) {
+    static int total{0};
+    return total += x;
+}
+
+int main() {
+    std::cout << accumulate(4) << '\n'; // prints 4
+    std::cout << accumulate(3) << '\n'; // prints 7
+    std::cout << accumulate(2) << '\n'; // prints 9
+    std::cout << accumulate(1) << '\n'; // prints 10
+
+    return 0;
+}
